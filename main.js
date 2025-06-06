bcea09fae573ed93b1235e67724c3a23d80556e5
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-06-06 14:41:24 -0500

    base plate gui

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello from Electron 👋')
+//2 modules imported:
+//app: controls app's even lifecycle
+//BrowserWindow, creates and manages app windows
+const { app, BrowserWindow } = require('electron')//import needed modules
+
+//reusable function to insantiate windows
+//loads web page into new BrowserWindow instance
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  win.loadFile('index.html')
+}
+
+//calls createWindow when app is ready
+//whenReeady looks like an async emitter thing
+app.whenReady().then(() => {
+  createWindow()
+})
