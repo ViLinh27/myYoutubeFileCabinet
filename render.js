d79f8f0718d56db8a6d3247b664ea49f75182234
Vi-Linh Nguyen <vilinh27@gmail.com>
2025-06-09 17:39:27 -0500

    base plate styling to fix later
    still going through tutorial

--- render.js
+++ render.js
@@ -1,3 +1,17 @@
 //renderer process
 //renders UI and handles user interactions
 //where the filing cabinet will live
+//UI logic
+const channelNameInput = document.getElementById('channelName');
+const channelLinkInput = document.getElementById('channelLink');
+const channelCategoryInput = document.getElementById('channelCategory');
+const addChannelBtn = document.getElementById('addChannelBtn');
+const categoriesContainer = document.getElementById('categoriesContainer');
+
+let channels = []; //in-memory array ot hold channel data
+
+//make a unique ID for each channel entry
+function generateUniqueId(){
+    //edit later
+    return '-'+Math.random().toString(36).substr(2,9);
+}
